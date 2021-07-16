693f7b17f1586923c41311005b1cc143601918d5
Piyush <impiyush016@gmail.com>
2021-07-16 20:44:03 +0530

    Added ToDo App

---
+++ todo/task/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path
+from .views import (
+    RegisterPage,
+    TaskList,
+    TaskDetail,
+    TaskCreate,
+    TaskUpdate,
+    TaskDelete,
+    CustomLoginView,
+)
+from django.contrib.auth.views import LogoutView
+
+app_name = "task"
+urlpatterns = [
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="task:login"), name="logout"),
+    path("register/", RegisterPage.as_view(), name="register"),
+    path("", TaskList.as_view(), name="taskList"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("create-task/", TaskCreate.as_view(), name="task-create"),
+    path("task-update/<int:pk>", TaskUpdate.as_view(), name="task-update"),
+    path("task-delete/<int:pk>", TaskDelete.as_view(), name="task-delete"),
+]
